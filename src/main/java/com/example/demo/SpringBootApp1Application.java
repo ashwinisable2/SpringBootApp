086a0c1338c36b2a3341c274c53eb0ad7b75c367
Ashwini <ashwinikanawade91@gmail.com>
2022-09-22 04:48:29 +0530

    HIS-300:Related changes

--- src/main/java/com/example/demo/SpringBootApp1Application.java
+++ src/main/java/com/example/demo/SpringBootApp1Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootApp1Application {
 
+private static final string REST_URL="https://www.equifax.com/getScores";
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp1Application.class, args);
 		int i=10;
