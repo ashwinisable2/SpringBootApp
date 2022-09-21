9f1f316889aa36403452df2be77bc1499abdd46b
Ashwini <ashwinikanawade91@gmail.com>
2022-09-22 05:14:08 +0530

    HIS-301:Related changes

--- src/main/java/com/example/demo/SpringBootApp1Application.java
+++ src/main/java/com/example/demo/SpringBootApp1Application.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootApp1Application {
-
+	private Map<String,Object> cache = new HashMap<String,Object>()
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp1Application.class, args);
 		int i=10;
+	}
+	//HIS-301 Changes
+	public void loadDataTOCache()
+	{
+		//logic
 	}
 
 }
